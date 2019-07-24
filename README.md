3d79eaa47f53a123778134f7d5768399e5e434c8
jomar.cardoso <Jo5592720>
2019-07-24 13:09:11 -0300

    sample

---
+++ README.md
@@ -0,0 +1,11 @@
+# Converter JSON para XML
+
+- Busca de uma api
+- converte para xml
+- salva um arquivo
+
+## Para rodar
+- npm start
+
+## Dependências
+- node.js
---
+++ index.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+const { json2xml } = require('./json2xml');
+const fs = require('fs');
+
+function save(data) {
+  fs.writeFile('pokemon.xml', data, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+
+    console.log('The file was saved!');
+  });
+}
+
+function converAndSave(data) {
+  console.log(data)
+  const convertedData = json2xml(JSON.parse(data));
+  save(convertedData);
+}
+
+fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  .then(res => res.text())
+  .then(converAndSave);
